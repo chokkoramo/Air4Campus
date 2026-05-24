c8f871df6376ed873ec9c30cf0c5d7844b15b229
JuanCaaa <perdomojuank300@gmail.com>
2026-05-24 18:08:35 -0500

    refactor: restructure project files and enhance ESP32 payload builder for sensor data processing

--- src/frameworks_drivers/sensors/esp32_payload.py
+++ src/frameworks_drivers/sensors/esp32_payload.py
@@ -1,6 +1,5 @@
 import time
 
-
 class ESP32PayloadBuilder:
     def __init__(self, device_id, classroom_id=None, classroom_type="theoretical"):
         self.device_id = device_id
@@ -8,10 +7,33 @@ class ESP32PayloadBuilder:
         self.classroom_type = classroom_type
 
     def build(self, sensor_readings):
-        return {
+        temp = None
+        hum = None
+        aq = None
+
+        for reading in sensor_readings:
+            if isinstance(reading, dict):
+                if "temperature" in reading:
+                    temp = reading["temperature"]
+                elif "temp" in reading:  
+                    temp = reading["temp"]
+                    
+                if "humidity" in reading:
+                    hum = reading["humidity"]
+                elif "hum" in reading:   
+                    hum = reading["hum"]
+                    
+                if "air_quality" in reading:
+                    aq = reading["air_quality"]
+
+        payload = {
             "device_id": self.device_id,
             "classroom_id": self.classroom_id,
             "classroom_type": self.classroom_type,
             "ts": time.time(),
-            "sensors": sensor_readings,
+            "temperature": temp,
+            "humidity": hum,
+            "air_quality": aq,
         }
+        
+        return payload
