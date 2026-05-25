40d9537abe62460a15554f5b8df0fdab1b8dba6f
JuanCaaa <perdomojuank300@gmail.com>
2026-05-24 19:10:57 -0500

    refactor: clean up JsonPresenter by removing unnecessary whitespace and ensuring consistent formatting

--- src/interface_adapters/presenters/json_presenter.py
+++ src/interface_adapters/presenters/json_presenter.py
@@ -1,5 +1,4 @@
 from typing import Any
-
 from entities.models import ComfortAnalysis
 
 
@@ -9,12 +8,28 @@ class JsonPresenter:
 
         for document in documents:
             item = dict(document)
+            
             if "_id" in item:
                 item["_id"] = str(item["_id"])
 
             ts = item.get("ts")
             if hasattr(ts, "timestamp"):
                 item["ts"] = int(ts.timestamp() * 1000)
+
+            if "sensors" in item and isinstance(item["sensors"], list):
+                for sensor in item["sensors"]:
+                    if "temperature" in sensor:
+                        item["temperature"] = sensor.get("temperature")
+                    if "humidity" in sensor:
+                        item["humidity"] = sensor.get("humidity")
+                    if "air_quality" in sensor:
+                        item["air_quality"] = sensor.get("air_quality")
+                
+                item.pop("sensors", None)
+            
+            item.setdefault("temperature", None)
+            item.setdefault("humidity", None)
+            item.setdefault("air_quality", None)
 
             data.append(item)
 
